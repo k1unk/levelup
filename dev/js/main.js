0493c3ee9e6b3d62272e5196c1853671a7c1c590
k1unk <klunkamel@mail.ru>
2022-11-27 09:12:15 +0300

    Initial commit

---
+++ dev/js/main.js
@@ -0,0 +1,38 @@
+let btns = document.getElementsByClassName('order__button');
+for (let i = 0; i < btns.length; ++i) {
+    btns[i].addEventListener('click', function (e) {
+        let blockToScroll = document.getElementById("order-block").offsetTop;
+        window.scrollTo({top: blockToScroll, behavior: 'smooth'});
+    });
+}
+
+let startTimer = () => {
+    let timerSeconds = 1800
+    let getTimerString = () => {
+        let minutes = Math.floor((timerSeconds) / 60)
+        let seconds = timerSeconds % 60
+        if (seconds < 10) seconds = `0${seconds}`
+        return `${minutes}:${seconds}`
+    }
+
+    let timer = setInterval(() => {
+        if (timerSeconds === 0) {
+            clearInterval(timer);
+        }
+        document.getElementById("timer").innerHTML = getTimerString();
+        timerSeconds--
+    }, 1000);
+
+}
+
+let timerStarted = false
+window.onscroll = function (e) {
+    let scrollToElem = document.getElementById("order-block").offsetTop - window.innerHeight
+    let winScrollTop = document.documentElement.scrollTop;
+    if (winScrollTop > scrollToElem) {
+        if (!timerStarted) {
+            timerStarted = true
+            startTimer()
+        }
+    }
+};
